78c051968269792222de7b304a6b36660bed8869
gaoyang820 <gaoyang1851948@gmail.com>
2026-04-22 15:46:57 +0800

    01357129高揚

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -3,5 +3,6 @@ public class FirstClass4Team{
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號: \"01357108\"\n姓名: \"柯岳昇\"");
         System.out.println("\"學號: \"01357137\"\n姓名: \"蔡承翰\"");
+        System.out.println("\"學號: \"01357129\"\n姓名: \"高揚\"");
     }
 }
