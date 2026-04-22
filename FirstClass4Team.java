dd4bdfc552ae85e61956d59979ee88d9ee869ebe
bigggwatermelon <bigwatermelon0601@gmail.com>
2026-04-22 15:35:14 +0800

    01357137

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -2,5 +2,6 @@ public class FirstClass4Team{
     public static void main(String[] args){
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號: \"01357108\"\n姓名: \"柯岳昇\"");
+        System.out.println("\"學號: \"01357137\"\n姓名: \"蔡承翰\"");
     }
 }
